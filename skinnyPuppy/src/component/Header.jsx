930fdcbf7bfaf81e71342c0a251f6eef648dc10a
Kwabena Anane Kwarteng <kwabena.ananeK@gmail.com>
2025-03-14 11:21:34 +0100

    Erase index.css & change logo color

--- skinnyPuppy/src/component/Header.jsx
+++ skinnyPuppy/src/component/Header.jsx
@@ -1,18 +1,22 @@
-
-
 const Header = () => {
   return (
-    <div className="nav-bar">
-        <h1>Skinny Puppy</h1>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
+    <div className='nav-bar'>
+      <h1 className="logo">Skinny Puppy</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href='/index.html'>Home</a>
+          </li>
+          <li>
+            <a href='/about.html'>About</a>
+          </li>
+          <li>
+            <a href='/contact.html'>Contact</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
