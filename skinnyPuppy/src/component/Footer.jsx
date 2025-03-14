69e45ba928ba67c2a2faf873fb4b243f5bfbc45b
NiklasNilsson79 <nelzonproduction@gmail.com>
2025-03-14 10:39:29 +0100

    footer

---
+++ skinnyPuppy/src/component/Footer.jsx
@@ -0,0 +1,35 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Follow Skinny Puppy on:</p>
+      <ul>
+        <li>
+          <a
+            href="https://www.facebook.com/skinnypuppy/"
+            target="_blank"
+            rel="noopener noreferrer">
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/skinnypuppy"
+            target="_blank"
+            rel="noopener noreferrer">
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/skinnypuppyofficial/"
+            target="_blank"
+            rel="noopener noreferrer">
+            Instagram
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
